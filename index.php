5823fa06630a1a8d67fb6c59da2b0610009fd824
Carlos Alberto Machicado Cabezas <carlos.alberto.machicado@gmail.com>
2019-07-13 22:37:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
-echo 'hola mundo';
-
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hola Mundo!</title>
+    </head>
+    <body>
+        <h1>Hola Mundo!</h1>
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hola Mundo!</title>
+    </head>
+    <body>
+        <h1>Hola Mundo!</h1>
+    </body>
+</html>
 ?>
